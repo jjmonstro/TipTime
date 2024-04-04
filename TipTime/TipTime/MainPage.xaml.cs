261075b0677b752bfc9111c5c97a0e0dfc4b5690
jjmonstro <jpcorreia1211@gmail.com>
2024-04-04 12:44:25 -0300

    TXT commit

--- TipTime/TipTime/MainPage.xaml.cs
+++ TipTime/TipTime/MainPage.xaml.cs
@@ -9,16 +9,42 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void Percentage15Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            TipPercentageSlider.Value = 15;
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        private void Percentage20Button_Clicked(object sender, EventArgs e)
+        {
+            TipPercentageSlider.Value = 20;
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void RoundDownButton_Clicked(object sender, EventArgs e)
+        {
+            double valordaRefeição = Convert.ToDouble(ValueEntry.Text);
+            double porcentagemdaGorjeta = TipPercentageSlider.Value / 100;
+            double valordaGorjeta = valordaRefeição * porcentagemdaGorjeta;
+            valordaGorjeta = Math.Floor(valordaGorjeta);
+            double totalrefeição = valordaGorjeta + valordaRefeição;
+            TotalLabel.Text = totalrefeição.ToString();
+            TipPercentageLabel.Text = totalrefeição.ToString();
+        }
+
+        private void RoundUpButton_Clicked(object sender, EventArgs e)
+        {
+            double valordaRefeição = Convert.ToDouble(ValueEntry.Text);
+            double porcentagemdaGorjeta = TipPercentageSlider.Value / 100;
+            double valordaGorjeta = valordaRefeição * porcentagemdaGorjeta;
+            valordaGorjeta = Math.Ceiling(valordaGorjeta);
+            double totalrefeição = valordaGorjeta + valordaRefeição;
+            TotalLabel.Text = totalrefeição.ToString();
+            TipPercentageLabel.Text = totalrefeição.ToString();
+        }
+
+        private void OnTipPercentageSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double valordoSlide = TipPercentageSlider.Value;
+            TipPercentageLabel.Text = valordoSlide.ToString();
         }
     }
 
